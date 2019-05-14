77dbaef2a4c1499436caf6f10e0ecd4fd89c04c7
Manuel Alonso <manueldavid@live.com.mx>
2019-05-13 21:15:07 -0500

    Navbar Usuarios

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+ document.addEventListener('DOMContentLoaded', function () {
+
+
+     var elems = document.querySelectorAll('.sidenav');
+     var instances = M.Sidenav.init(elems);
+
+     var elems = document.querySelectorAll('.dropdown-trigger');
+     var instances = M.Dropdown.init(elems, {
+         inDuration: 300,
+         outDuration: 225,
+         hover: true, 
+         coverTrigger: false, 
+         alignment: 'right', 
+
+     });
+ });
