c7f24ec8d769d7d8041d87c4be81cb7c52ee21fb
Manuel Alonso <manueldavid@live.com.mx>
2019-05-13 23:10:56 -0500

    cambios en el navbar

--- js/navbar.js
+++ js/navbar.js
@@ -8,9 +8,11 @@
      var instances = M.Dropdown.init(elems, {
          inDuration: 300,
          outDuration: 225,
-         hover: true, 
-         coverTrigger: false, 
-         alignment: 'right', 
+         hover: true,
+         coverTrigger: false,
+         alignment: 'left',
 
      });
+     var el = document.querySelectorAll('.tabs');
+     var instance = M.Tabs.init(el);
  });
