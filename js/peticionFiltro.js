a3c483602f3d04cca7a30563a4a644be73ba75c0
Alonso <j_alonso1996@hotmail.com>
2019-09-12 10:01:09 -0500

    Filtro de busqueda
    Filtro de busqueda en hojas de trabajo terminado

--- js/peticionFiltro.js
+++ js/peticionFiltro.js
@@ -1,11 +1,25 @@
 $(filtro_busqueda());
 
-function filtro_busqueda(hoja){
+function filtro_busqueda(titulo,tipo,idioma){
     $.ajax({
-        url: '../php/busqueda-hoja.php',
+        url: 'php/busqueda-hoja.php',
         type: 'POST',
         dataType: 'html',
-        data: { hojas: hoja}
+        data: { 
+            titulo: titulo,
+            tipo: tipo,
+            idioma: idioma
+        }
     })
-
+    .done(function(resultado){
+        $("#filtro_resultado").html(resultado);
+    })
 }
+$(document).on('click','#busqueda',function()
+{
+    var titulo = document.getElementsByName("titulo")[0].value;
+    var tipo = document.getElementsByName("tipo")[0].value;
+    var idioma = document.getElementsByName("idioma")[0].value;
+
+    filtro_busqueda(titulo,tipo,idioma);
+});
---
+++ php/busqueda-hoja.php
@@ -0,0 +1,91 @@
+<?php 
+$server="localhost";
+$pass="";
+$user="root";
+$db="caadi";
+$conexion=mysqli_connect($server,$user,$pass,$db);
+
+if(!$conexion){
+    echo "Error. SIn conexion a la base de datos";
+    echo "Errno de depuracion ".mysqli_connect_errno().PHP_EOL;
+    echo "Error de depuracion ".mysqli_connect_error().PHP_EOL;
+}
+
+$hojas = "";
+$query = mysqli_query($conexion, "SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo  FROM hoja_trabajo JOIN nivel JOIN idioma WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma ");
+
+while($datos = mysqli_fetch_array($query)){
+    $idioma = $datos["nombre"];
+    $nivel = $datos["nivel"];
+    $titulo = $datos["titulo"];
+    $tipo = $datos["tipo"];
+    $hojas .= ' 
+    <li class="collection-item avatar">
+        <a href="#!" class="left"><i class="material-icons circle z-depth-1" id="descargar">file_download</i></a>
+        <span class="title"><?php echo $array["tema"] ?></span>
+        <a href="#!" class="right"><i class="material-icons contestar z-depth-1">file_upload</i></a>
+        <a href="../caadi/examen.html" class="right"><i class="material-icons contestar z-depth-1">exit_to_app</i></a>
+        <p>'.$titulo.'</p>
+        <p>'.$idioma.', '.$nivel.'
+            <br> '.$tipo.'
+            <a href="#!" class="right"><i class="material-icons eliminar z-depth-1">delete</i></a>
+        </p>
+    </li>';
+}
+
+if(isset($_POST['titulo']) || isset($_POST['tipo']) || isset($_POST['idioma'])){
+    $titulo = $_POST['titulo'];
+    $tipo = $_POST['tipo'];
+    $idioma = $_POST['idioma'];
+    if($titulo || $tipo ||$idioma != ""){
+        $hojas = " ";
+        if($titulo != "" && $tipo != "" && $idioma != ""){ //todos rellenados
+            $filtro = mysqli_query($conexion,"SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo FROM hoja_trabajo JOIN nivel JOIN idioma
+            WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma AND (hoja_trabajo.tema = '$titulo' AND hoja_trabajo.area = '$tipo' AND idioma.nombre = '$idioma')");
+        } 
+        elseif($titulo != "" && $tipo != "" && $idioma == ""){
+            $filtro = mysqli_query($conexion,"SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo FROM hoja_trabajo JOIN nivel JOIN idioma
+            WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma AND hoja_trabajo.tema = '$titulo' AND hoja_trabajo.area = '$tipo'");
+        }
+        elseif($titulo != "" && $tipo == "" && $idioma != ""){
+            $filtro = mysqli_query($conexion,"SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo FROM hoja_trabajo JOIN nivel JOIN idioma
+            WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma AND (hoja_trabajo.tema = '$titulo' AND idioma.nombre = '$idioma')");
+        }//
+        elseif($titulo != "" && $tipo == "" && $idioma == ""){
+            $filtro = mysqli_query($conexion,"SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo FROM hoja_trabajo JOIN nivel JOIN idioma
+            WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma AND (hoja_trabajo.tema = '$titulo')");
+        }
+        elseif($tipo != "" && $titulo == "" && $idioma != ""){
+            $filtro = mysqli_query($conexion,"SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo FROM hoja_trabajo JOIN nivel JOIN idioma
+            WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma AND (hoja_trabajo.area = '$tipo' AND idioma.nombre = '$idioma')");
+        }
+        elseif($tipo != "" && $titulo == "" && $idioma == ""){
+            $filtro = mysqli_query($conexion,"SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo FROM hoja_trabajo JOIN nivel JOIN idioma
+            WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma AND (hoja_trabajo.area = '$tipo')");
+        }
+        elseif($idioma != "" && $titulo == "" && $tipo == ""){
+            $filtro = mysqli_query($conexion,"SELECT idioma.nombre AS nombre, nivel.nivel AS nivel, hoja_trabajo.tema AS titulo, hoja_trabajo.area AS tipo FROM hoja_trabajo JOIN nivel JOIN idioma
+            WHERE hoja_trabajo.id_nivel = nivel.id_nivel AND nivel.id_idioma = idioma.id_idioma AND (idioma.nombre = '$idioma')");
+        }
+    
+        while($hojas_filtradas = mysqli_fetch_array($filtro)){
+            $tituloFiltrado = $hojas_filtradas['titulo'];
+            $idiomaFiltrado = $hojas_filtradas['nombre'];
+            $nivelFiltrado = $hojas_filtradas['nivel'];
+            $tipoFiltrado = $hojas_filtradas['tipo'];
+            $hojas .= '<li class="collection-item avatar">
+                <a href="#!" class="left"><i class="material-icons circle z-depth-1" id="descargar">file_download</i></a>
+                <span class="title"><?php echo $array["tema"] ?></span>
+                <a href="#!" class="right"><i class="material-icons contestar z-depth-1">file_upload</i></a>
+                <a href="../caadi/examen.html" class="right"><i class="material-icons contestar z-depth-1">exit_to_app</i></a>
+                <p>'.$tituloFiltrado.'</p>
+                <p>'.$idiomaFiltrado.', '.$nivelFiltrado.'
+                    <br> '.$tipoFiltrado.'
+                    <a href="#!" class="right"><i class="material-icons eliminar z-depth-1">delete</i></a>
+                </p>
+            </li>';
+        }
+    }
+}
+echo $hojas;
+?>
